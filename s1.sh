35ba64cc384374759e505bda5edc37d53887b095
kusumareddy <ec2-user@ip-172-31-12-143.ap-south-1.compute.internal>
2024-08-06 14:58:00 +0000

    s1 is modified

--- s1.sh
+++ s1.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+echo "enter the username"
+read username
+echo "enter the password"
+read password
+if [ $usename = "admin" -a $password = "admin123" -o $username = "developer" -a $password = "javapython@123" ];
+then
+echo "condition is successful"
+else 
+echo "condition is not successful"
+fi
+
 echo "enter the age"
 read age
-if [ $age -lt 12 ];
+if [ $age -lt 13 ];
+then 
+echo "child"
+elif [ $age -gt 12 -a $age -lt 20 ];
 then
-echo "display as child"
-elif [ $age -gt 13 -a $age -lt 19 ];
+echo "teenaer"
+elif [ $age -gt 19 ];
 then
-echo "display as teenagers"
-elif [ $age -ge 20 ];
-then
-echo "display as adult"
+echo "adult"
+elif [ $age -gt 60 ];
+then 
+echo "oldage"
 else
 echo "invalid statement"
 fi
-
